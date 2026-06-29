74865ba9ae0dba43f9b9e9641492d6a24e674e08
Cortex-Forest <ycx868686@gmail.com>
2026-06-30 00:18:58 +0800

    v0.5 Transaction Module

---
+++ src/transaction.rs
@@ -0,0 +1,16 @@
+#[derive(Debug, Clone)]
+pub struct Transaction {
+    pub from: String,
+    pub to: String,
+    pub amount: u64,
+}
+
+impl Transaction {
+    pub fn new(from: String, to: String, amount: u64) -> Self {
+        Self {
+            from,
+            to,
+            amount,
+        }
+    }
+}
